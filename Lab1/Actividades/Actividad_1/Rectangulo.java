acaf41386141f95c91a04c2b70ce9bbe4c69eec3
AndreeAguilar15 <143722715+AndreeAguilar15@users.noreply.github.com>
2026-03-15 20:10:28 -0500

    Create Rectangulo.java

---
+++ Lab1/Actividades/Actividad_1/Rectangulo.java
@@ -0,0 +1,38 @@
+public class Rectangulo {
+    private Coordenada esquina1;
+    private Coordenada esquina2;
+
+    public Rectangulo(Coordenada c1, Coordenada c2) {
+        setEsquina1(c1);
+        setEsquina2(c2);
+    }
+
+    public void setEsquina1(Coordenada coo) {
+        this.esquina1 = new Coordenada(coo);
+    }
+
+    public void setEsquina2(Coordenada coo) {
+        this.esquina2 = new Coordenada(coo);
+    }
+
+    public Coordenada getEsquina1() {
+        return this.esquina1;
+    }
+
+    public Coordenada getEsquina2() {
+        return this.esquina2;
+    }
+
+    public double calculoArea() {
+        double base = Math.abs(esquina1.getX() - esquina2.getX());
+        double altura = Math.abs(esquina1.getY() - esquina2.getY());
+        return base * altura;
+    }
+
+    public String toString() {
+        return "([" + Math.min(esquina1.getX(), esquina2.getX()) + ", " +
+                Math.min(esquina1.getY(), esquina2.getY()) + "], [" +
+                Math.max(esquina1.getX(), esquina2.getX()) + ", " +
+                Math.max(esquina1.getY(), esquina2.getY()) + "])";
+    }
+}
